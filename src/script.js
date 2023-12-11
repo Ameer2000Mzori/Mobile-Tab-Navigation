1d044f89065514ac541e3bdf4b4b1ec1f2d068de
Ameer <ameerapex496@gmail.com>
2023-12-11 03:46:02 +0100

    added page one and two func

--- src/script.js
+++ src/script.js
@@ -9,28 +9,108 @@ var pageFour = false;
 // our images :
 // 1 https://images.unsplash.com/photo-1682905926517-6be3768e29f0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
 // functions
-var changeScreen = function (indexNum) {
+var changeScreen = function (indexNum, el) {
     console.log(indexNum);
     if (indexNum === 1) {
         if (pageOne) {
             console.log("page one is already true");
         }
         else {
+            // set false to each page has removed / add true to page clicked
             pageOne = true;
-            screenImg.src =
-                "https://images.unsplash.com/photo-1682905926517-6be3768e29f0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-            console.log("just made page one true");
-            screenImg.classList.add("active");
+            pageTwo = false;
+            pageThree = false;
+            pageFour = false;
+            screenImg.classList.remove("active");
+            for (var i = 0; i < lisEl.length; i++)
+                lisEl.forEach(function (el) {
+                    el.classList.remove("active");
+                });
+            // set time out for effects to run on page
+            setTimeout(function () {
+                screenImg.src =
+                    "https://images.unsplash.com/photo-1682905926517-6be3768e29f0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+                console.log("just made page one true");
+                screenImg.classList.add("active");
+                el.classList.add("active");
+            }, 200);
         }
     }
     else if (indexNum === 2) {
+        if (pageTwo) {
+            console.log("page one is already true");
+        }
+        else {
+            // set false to each page has removed / add true to page clicked
+            pageOne = false;
+            pageTwo = true;
+            pageThree = false;
+            pageFour = false;
+            screenImg.classList.remove("active");
+            for (var i = 0; i < lisEl.length; i++)
+                lisEl.forEach(function (el) {
+                    el.classList.remove("active");
+                });
+            // set time out for effects to run on page
+            setTimeout(function () {
+                screenImg.src =
+                    "https://images.unsplash.com/photo-1701743801555-5e4a58185b24?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+                console.log("just made page one true");
+                screenImg.classList.add("active");
+                el.classList.add("active");
+            }, 200);
+        }
         console.log("wow 2 ");
     }
     else if (indexNum === 3) {
-        console.log("wow 3 ");
+        if (pageThree) {
+            console.log("page one is already true");
+        }
+        else {
+            // set false to each page has removed / add true to page clicked
+            pageOne = false;
+            pageTwo = false;
+            pageThree = true;
+            pageFour = false;
+            screenImg.classList.remove("active");
+            for (var i = 0; i < lisEl.length; i++)
+                lisEl.forEach(function (el) {
+                    el.classList.remove("active");
+                });
+            // set time out for effects to run on page
+            setTimeout(function () {
+                screenImg.src =
+                    "https://images.unsplash.com/photo-1702140944199-5c321e5d5798?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+                console.log("just made page one true");
+                screenImg.classList.add("active");
+                el.classList.add("active");
+            }, 200);
+        }
     }
     else if (indexNum === 4) {
-        console.log("wow 4 ");
+        if (pageFour) {
+            console.log("page one is already true");
+        }
+        else {
+            // set false to each page has removed / add true to page clicked
+            pageOne = false;
+            pageTwo = false;
+            pageThree = false;
+            pageFour = true;
+            screenImg.classList.remove("active");
+            for (var i = 0; i < lisEl.length; i++)
+                lisEl.forEach(function (el) {
+                    el.classList.remove("active");
+                });
+            // set time out for effects to run on page
+            setTimeout(function () {
+                screenImg.src =
+                    "https://images.unsplash.com/photo-1702157449974-7bf9e33409c5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+                console.log("just made page one true");
+                screenImg.classList.add("active");
+                el.classList.add("active");
+            }, 200);
+        }
     }
     else {
         console.log("wow else");
@@ -39,10 +119,8 @@ var changeScreen = function (indexNum) {
 // event lisnters
 lisEl.forEach(function (el, indx) {
     el.addEventListener("click", function (e) {
-        el.classList.add("active");
         var indexNum = indx + 1;
-        changeScreen(indexNum);
+        changeScreen(indexNum, el);
     });
 });
 // when windows loads
-changeScreen(1);
